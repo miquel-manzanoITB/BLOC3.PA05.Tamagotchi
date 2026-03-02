5763dd0eda3506a1fc670048b75259443dba84d1
miquel-manzano <miquel.manzano.7e6@itb.cat>
2026-03-02 13:35:07 +0100

    feat: stats error control #5

--- BLOC3.PA05.Tamagotchi/BLOC3.PA05.Tamagotchi/Models/Stats.cs
+++ BLOC3.PA05.Tamagotchi/BLOC3.PA05.Tamagotchi/Models/Stats.cs
@@ -1,14 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace BLOC3.PA05.Tamagotchi.Models
 {
     public class Stats
     {
-        public int HealthLevel { get; set; }
-        public int EnergyLevel { get; set; }
-        public int HungryLevel { get; set; }
+        private int healthLevel;
+        public int HealthLevel
+        {
+            get => healthLevel;
+            set => healthLevel = Math.Clamp(value, 0, 100);
+        }
+
+        private int energyLevel;
+        public int EnergyLevel
+        {
+            get => energyLevel;
+            set => energyLevel = Math.Clamp(value, 0, 100);
+        }
+        private int hungryLevel;
+        public int HungryLevel
+        {
+            get => hungryLevel;
+            set => hungryLevel = Math.Clamp(value, 0, 100);
+        }
 
         public Stats()
         {
@@ -16,5 +29,11 @@ namespace BLOC3.PA05.Tamagotchi.Models
             EnergyLevel = 100;
             HungryLevel = 100;
         }
+        public void AddHealth(int amount) => HealthLevel += amount;
+        public void AddEnergy(int amount) => EnergyLevel += amount;
+        public void AddHungry(int amount) => HungryLevel += amount;
+        public void RemoveHealth(int amount) => HealthLevel -= amount;
+        public void RemoveEnergy(int amount) => EnergyLevel -= amount;
+        public void RemoveHungry(int amount) => HungryLevel -= amount;
     }
 }
