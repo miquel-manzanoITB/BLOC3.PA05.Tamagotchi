0fa0d3febd8f4e7c46855e33f40f100631d411d8
miquel-manzano <miquel.manzano.7e6@itb.cat>
2026-03-02 13:39:16 +0100

    feat: implement IPlay, ISleep and IEat on animal model

--- BLOC3.PA05.Tamagotchi/BLOC3.PA05.Tamagotchi/Models/Animal.cs
+++ BLOC3.PA05.Tamagotchi/BLOC3.PA05.Tamagotchi/Models/Animal.cs
@@ -1,15 +1,30 @@
 ﻿using BLOC3.PA05.Tamagotchi.Models.Enumerations;
+using BLOC3.PA05.Tamagotchi.Models.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BLOC3.PA05.Tamagotchi.Models
 {
-    public class Animal : Pet
+    public class Animal : Pet, IPlay, ISleep, IEat
     {
         public Animal(string name, EmotionState emotion) : base(name, emotion)
         {
 
         }
+
+        public void Eat()
+        {
+            PetStats.RemoveHungry(20);
+            PetStats.AddEnergy(10);
+        }
+
+        public void Play()
+        {
+            PetStats.RemoveEnergy(20);
+            Emotion = EmotionState.Happy;
+        }
+
+        public void Sleep() => PetStats.AddEnergy(100);
     }
 }
