7dd73bcc56e29cdde347c4133f2c2921b5b96669
miquel-manzano <miquel.manzano.7e6@itb.cat>
2026-03-02 12:43:08 +0100

    feat: create IEat, IPlay and ISleep interfaces

---
+++ BLOC3.PA05.Tamagotchi/BLOC3.PA05.Tamagotchi/Models/Interfaces/IPlay.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLOC3.PA05.Tamagotchi.Models.Interfaces
+{
+    public interface IPlay
+    {
+        void Play();
+    }
+}
